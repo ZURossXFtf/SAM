e1f61f367bc615a163ad261397cd01bdaacb9248
sempai <sempai@DESKTOP-2A94TS2>
2023-05-24 12:55:34 +0200

    Добавьте файлы проекта.

---
+++ 312/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] Z = new int[4];
+Random rnd = new Random();
+
+Console.Write("Массив: ");
+
+for (int i = 0; i < Z.Length; i++)
+{
+    Z[i] = rnd.Next(-10, 10);
+    Console.Write(Z[i] + " ");
+}
+
+Console.WriteLine();
+
+int S = Z.Where(x => x % 2 == 0 && x < 3).Sum();
+int P = Z.Where(x => x % 2 != 0 && x > 1).Sum();
+int R = P + S;
+Console.WriteLine("S = " + S);
+Console.WriteLine("P = " + P);
+Console.WriteLine("R = " + R);
